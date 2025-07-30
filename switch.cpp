bcaedd56a25b9515edbdd20bd6cbe3c481d0179e
varuna-code7 <muthuvaruna4@gmail.com>
2025-07-30 22:19:08 +0530

    Update switch.cpp

--- switch.cpp
+++ switch.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+using namespace std;
 
+// Function to add two numbers
+int add(int x, int y) {
+    return x + y;
+}
+
+int main() {
+    int a, b;
+
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+
+    // Calling the function
+    int sum = add(a, b);
+
+    cout << "Sum = " << sum << endl;
+    return 0;
+}
+  

8f642c6c5eabc256173b8f013c467acdd947fe80
varuna-code7 <muthuvaruna4@gmail.com>
2025-07-30 22:02:23 +0530

    function.cpp

---
+++ function.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+// Function to add two numbers
+int add(int x, int y) {
+    return x + y;
+}
+
+int main() {
+    int a, b;
+
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+
+    // Calling the function
+    int sum = add(a, b);
+
+    cout << "Sum = " << sum << endl;
+    return 0;
+}
